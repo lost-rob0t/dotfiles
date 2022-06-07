3d43fd44f4608b8dfc169bb478c98528f400317a
N545PY <nsaspy@airmail.cc>
2022-06-07 01:23:33 -0400

    add syncthing

--- nixos/configurations/neptune/services.nix
+++ nixos/configurations/neptune/services.nix
@@ -55,6 +55,13 @@
  touchegg = {
  enable = true;
 };
+ syncthing = {
+   enable = true;
+   user = "unseen";
+   dataDir = "/home/unseen/Documents";    # Default folder for new synced folders
+   configDir = "/home/unseen/Documents/.config/syncthing";   # Folder for Syncthing's settings and keys
+ };
+
 };
 virtualisation = {
     podman = {
