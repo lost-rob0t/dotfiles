70fc292b915448c357b0463ad837a404905347e3
N545PY <nsaspy@airmail.cc>
2023-03-14 19:12:18 -0400

    sync

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,19 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bongo-enabled-backends '(mpv))
- '(custom-safe-themes
-   '("da53441eb1a2a6c50217ee685a850c259e9974a8fa60e899d393040b4b8cc922" "c5ded9320a346146bbc2ead692f0c63be512747963257f18cc8518c5254b7bf5" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
- '(ignored-local-variable-values
-   '((org-docco-doccoize-me . t)
-     (org-export-html-style-include-default)
-     (org-export-html-postamble)))
  '(package-selected-packages
-   '(visual-fill-column use-package org-roam htmlize mastodon org-present flycheck-nim org-pomodoro vterm quelpa-use-package))
- '(warning-suppress-types
-   '((emacs)
-     (tabulated-list-revert-hook)
-     (tabulated-list-revert-hook))))
+   '(xclip w3m vterm pylint pandoc lsp-pyright direnv use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
