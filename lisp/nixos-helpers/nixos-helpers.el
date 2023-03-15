78c8f4903724b37144fd7ecd7dc5a87abe7ccd14
nsaspy <nsaspy@airmail.cc>
2023-03-15 16:51:56 -0400

    add nixos-helpers
    I havent done anything with it yet.

---
+++ lisp/nixos-helpers/nixos-helpers.el
@@ -0,0 +1,26 @@
+;;; nixos-helpers.el Helper functions to use with nixos -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023
+;;
+;; Author:  <unseen@flake>
+;; Maintainer:  <unseen@flake>
+;; Created: March 15, 2023
+;; Modified: March 15, 2023
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/unseen/nixos-helpers
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;; Helper functions to help managing nixos from emacs
+;;
+;;
+;;; Code:
+(require 'async)
+
+;; TODO work on nixos-helpers
+
+(provide 'nixos-helpers)
+;;; nixos-helpers.el ends here
