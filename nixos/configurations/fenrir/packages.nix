2d20d383d649a48744355e420135e4810f57e349
nsaspy <nsaspy@airmail.cc>
2023-04-09 16:36:29 -0400

    disable steam stuff

--- nixos/configurations/fenrir/packages.nix
+++ nixos/configurations/fenrir/packages.nix
@@ -153,8 +153,8 @@
   };
   programs.steam = {
     enable = false;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
   };
   programs.kdeconnect = {
    enable = true;
