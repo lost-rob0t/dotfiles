46978cafde464e9d4d70950e4df5687b014fee84
N545PY <nsaspy@airmail.cc>
2023-04-28 10:16:36 -0400

    add cve search engine

--- .config/nyxt/search-engines.lisp
+++ .config/nyxt/search-engines.lisp
@@ -2,6 +2,12 @@
 
 ;; Define buffer search-engines slot to be a list of several
 ;; nx-search-engines-provided ones.
+;;
+;(define-search-engine cve-mitre
+;  (:shortcut "cve"
+;   :fallback-url (quri:uri "https://cve.mitre.org/cve/search_cve_list.html")
+;   :base-search-url "https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=~a"
+;   :documentation "Search the CVE database. mitre allows you to search them by keyword"))
 (define-configuration (buffer web-buffer)
   ((search-engines (list (engines:google :shortcut "gmaps"
                                          :object :maps)
@@ -17,4 +23,8 @@
                                              :install-reminders nil
                                              :install-duckduckgo nil)
                          (engines:github :object :advanced :shortcut "git")
-                         (engines:brave :shortcut "b")))))
+                         (engines:brave :shortcut "b")
+                         (nyxt:make-search-engine "cve" "https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=~a" "https://cve.mitre.org/cve/search_cve_list.html")
+
+                         (engines:brave :shortcut "b")
+))))
