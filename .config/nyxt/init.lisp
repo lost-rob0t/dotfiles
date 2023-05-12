62980d61b340072de6ce4ff355c1eadc7c04e8a7
nsaspy <nsaspy@airmail.cc>
2023-05-12 02:39:50 -0400

    add slynk

--- .config/nyxt/init.lisp
+++ .config/nyxt/init.lisp
@@ -1,3 +1,31 @@
+#-quicklisp
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                         (user-homedir-pathname))))
+    (when (probe-file quicklisp-init)
+      (load quicklisp-init))))
+
+(ql:quickload :slynk)
+
+(in-package :nyxt)
+
+(when (asdf:load-system :slynk)
+  (define-command start-slynk (&optional (slynk-port *swank-port*))
+    "Start a Slynk server that can be connected to, for instance, in
+Emacs via SLY.
+
+Warning: This allows Nyxt to be controlled remotely, that is, to
+execute arbitrary code with the privileges of the user running Nyxt.
+Make sure you understand the security risks associated with this
+before running this command."
+    (slynk:create-server :port slynk-port :dont-close t)
+      (echo "Slynk server started at port ~a" slynk-port)))
+
+
+
+
+
 (in-package #:nyxt-user)
 
 
