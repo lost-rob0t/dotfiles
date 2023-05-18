64f32cd1f40ade8bca2b253c663057d21472b544
N545PY <nsaspy@airmail.cc>
2023-05-18 15:15:23 -0400

    sync

---
+++ .doom.d/snippets/emacs-lisp-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `emacs-lisp-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'emacs-lisp-mode
+                     '(("SPC-P" "(package! \"${1:name-of-package}\" :recipe (:type ${2:git} :host github :repo \"${3:user/repo}\"))" "package-github" t nil nil "/home/unseen/.doom.d/snippets/emacs-lisp-mode/package-github" nil "package-github")))
+
+
+;;; Do not edit! File generated at Tue Apr 18 16:20:30 2023
