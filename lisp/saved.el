bb131c2c22d445f0191ff953025ed877b7e12722
nsaspy <nsaspy@airmail.cc>
2023-07-13 20:47:25 -0400

    emacs: update run emacs script function
    Nolonger pops 2 buffer windows

--- lisp/saved.el
+++ lisp/saved.el
@@ -103,14 +103,14 @@ strings."
 
 
 
-(defun run-emacs-script ()
+(defun nsaspy/emacs-script ()
   "Run a emacs script in batch mode"
   (interactive)
   (let* ((file (read-file-name "Script to run: " default-directory))
          (buffer (get-buffer-create "*emacs-script*"))
          (args (read-string "Arguments: " nil t)))
-    (async-shell-command (format "%s --script %s %s" (executable-find "emacs") file args) buffer buffer)
-    (switch-to-buffer buffer)))
+    (async-shell-command (format "%s --script %s %s" (executable-find "emacs") file args) buffer buffer)))
+
 
 (defun nsaspy/wget-region
     (start end)
