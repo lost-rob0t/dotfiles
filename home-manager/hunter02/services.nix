59331809d32b798ad4d1cdaf9478a40bbd3feee6
nsaspy <nsaspy@fedora.email>
2023-08-10 12:41:42 -0400

    home-manager: add syncthing for hunter02

--- home-manager/hunter02/services.nix
+++ home-manager/hunter02/services.nix
@@ -8,5 +8,11 @@
     emacs = {
       enable = true;
     };
+    syncthing = {
+      enable = true;
+      user = "unseen";
+      dataDir = "/home/unseen/Documents"; # Default folder for new synced folders
+      configDir = "/home/unseen/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
+    };
   };
 }
