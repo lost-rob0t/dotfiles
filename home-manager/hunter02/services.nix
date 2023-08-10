e443f1f5bddac277c8793211fcd082c6bdd1d717
nsaspy <nsaspy@fedora.email>
2023-08-10 13:21:35 -0400

    home-manager: reminder that i need a hook to check before push

--- home-manager/hunter02/services.nix
+++ home-manager/hunter02/services.nix
@@ -12,7 +12,6 @@
       enable = true;
       user = "unseen";
       dataDir = "/home/unseen/Documents"; # Default folder for new synced folders
-      configDir = "/home/unseen/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
     };
   };
 }
