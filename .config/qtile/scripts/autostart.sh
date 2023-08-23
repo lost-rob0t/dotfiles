8208783b1a53464296d37701ffd50fc07541f133
nsaspy <nsaspy@fedora.email>
2023-08-23 14:46:33 -0400

    qtile: update picom config.

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -48,7 +48,7 @@ run nm-applet &
 run xfce4-power-manager &
 numlockx on &
 blueman-applet &
-picom --config $HOME/.config/qtile/scripts/picom.conf &
+picom --config $HOME/.config/picom/picom.conf  &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 spice-vdagent &
