2526101cb8fbd288d50e939481ff2b0b40b1cfd7
nsaspy <nsaspy@airmail.cc>
2024-04-04 20:47:53 -0400

    add annon.sh

---
+++ .config/qtile/scripts/anon.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+# Kill unsafe apps for anon use.
+killall -q discord brave  chrome dropbox skype icedove thunderbird firefox firefox-esr chromium xchat hexchat transmission steam firejail /usr/lib/firefox-esr/firefox-esr
