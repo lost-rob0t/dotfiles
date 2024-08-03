d5163f69337466290a7e1005c53e29c4d9047415
N545PY <nsaspy@fedora.email>
2024-08-03 17:47:31 -0400

    update flake

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -15,16 +15,27 @@
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
    '(simple-httpd color-theme-sanityinc-tomorrow org-roam htmlize xclip w3m vterm pylint pandoc lsp-pyright direnv use-package))
- '(safe-local-variable-values '((eval require 'org-make-toc)))
+ '(safe-local-variable-values
+   '((package . RFC2388)
+     (Package . CL-UNICODE)
+     (Package . FLEXI-STREAMS)
+     (Base . 10)
+     (Package . CHUNGA)
+     (Syntax . COMMON-LISP)
+     (eval require 'org-make-toc)))
  '(warning-suppress-types
-   '(
-     (emacs)
+   '((emacs)
      (emacs)
      (browse-url)
      (browse-url)
      (browse-url)
      (defvaralias))))
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 ;; custom-set-faces was added by Custom.
 ;; If you edit it by hand, you could mess it up, so be careful.
 ;; Your init file should contain only one such instance.
