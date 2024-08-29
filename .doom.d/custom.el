4d5bba879d3fee1cbb2f19ce235e72a9298dfdad
N545PY <nsaspy@fedora.email>
2024-08-29 01:13:42 -0400

    emacs: cleanup custome.el

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -24,18 +24,14 @@
      (Syntax . COMMON-LISP)
      (eval require 'org-make-toc)))
  '(warning-suppress-types
-   '((emacs)
-     (emacs)
-     (browse-url)
-     (browse-url)
-     (browse-url)
+   '((browse-url)
      (defvaralias))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-faces)
+;; custom-set-faces was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+
 ;; custom-set-faces was added by Custom.
 ;; If you edit it by hand, you could mess it up, so be careful.
 ;; Your init file should contain only one such instance.
