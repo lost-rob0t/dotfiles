9118d6a4aced24c2ed81cfc65497f46213a5e140
N545PY <nsaspy@fedora.email>
2025-02-07 13:08:45 -0500

    nix: remove old systems

--- flake.nix
+++ flake.nix
@@ -106,24 +106,7 @@
 
           ];
         };
-        fenrir = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [
-            # > Our main nixos configuration file <
-            ./nix/nixos-systems/fenrir/fenrir.nix
-            nixos-hardware.nixosModules.common-gpu-intel-disable
-          ];
-        };
-        kiosk-top = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [
-            # > Our main nixos configuration file <
-            ./nix/nixos-systems/kiosk-top/kiosk-top.nix
-            nixos-hardware.nixosModules.common-gpu-intel-disable
-          ];
-        };
       };
-
       # Standalone home-manager configuration entrypoint
       # Available through 'home-manager --flake .#your-username@your-hostname'
       homeConfigurations = {
@@ -136,25 +119,12 @@
             # TODO Import all the stuff HERE
           ];
         };
-        "unseen@phone" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs outputs; };
-          modules = [
-            ./nix/home-manager/systems/phone/home.nix
-          ];
-        };
+
         "unseen@hunter02" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
           extraSpecialArgs = { inherit inputs outputs; };
           modules = [
             ./nix/home-manager/systems/systems/hunter02/home.nix
-          ];
-        };
-        "unseen@fenrir" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs outputs; };
-          modules = [
-            ./nix/home-manager/systems/fenrir/home.nix
           ];
         };
       };
--- nix/nixos-systems/fenrir/misc.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  fonts.fonts = with pkgs; [
-  (nerdfonts.override { fonts = [ "Hack"]; })
-  jetbrains-mono
-
-];}
