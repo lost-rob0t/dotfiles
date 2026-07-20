83bbc4ed28d2cec38473dd2c08662c451ed78f43
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-19 22:08:40 -0400

    build graphical LXQt Logos installer ISO

--- nix/nixos/installer/logos-iso.nix
+++ nix/nixos/installer/logos-iso.nix
@@ -1,16 +1,53 @@
-{ self, lib, pkgs, modulesPath, ... }:
+{
+  self,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
+let
+  system = pkgs.stdenv.hostPlatform.system;
+  installer = self.packages.${system}.install-logos;
+  installerGui = self.packages.${system}.install-logos-gui;
+  installerDesktop = pkgs.makeDesktopItem {
+    name = "logos-installer";
+    desktopName = "Install Logos";
+    comment = "Install Logos with encrypted Btrfs";
+    exec = "${installerGui}/bin/install-logos-gui";
+    icon = "system-software-install";
+    terminal = false;
+    categories = [ "System" ];
+  };
+in
 {
   imports = [
-    (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+    (modulesPath + "/installer/cd-dvd/installation-cd-graphical-base.nix")
   ];
 
   networking.hostName = "logos-installer";
   isoImage.isoBaseName = lib.mkForce "logos-installer";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-  services.openssh.enable = true;
+  services = {
+    openssh.enable = true;
+
+    xserver.desktopManager.lxqt.enable = true;
+
+    displayManager = {
+      sddm.enable = true;
+      defaultSession = "lxqt";
+      autoLogin = {
+        enable = true;
+        user = "nixos";
+      };
+    };
+  };
+
   security.sudo.wheelNeedsPassword = false;
 
   environment = {
@@ -22,19 +59,23 @@
       dosfstools
       git
       gptfdisk
+      jq
       parted
       rsync
       util-linux
-
-      (writeShellScriptBin "provision-logos" ''
-        exec ${self.outPath}/scripts/provision-logos-disk.sh "$@"
-      '')
-
-      (writeShellScriptBin "install-logos" ''
-        exec ${self.outPath}/scripts/install-logos.sh "$@"
-      '')
+      installer
+      installerGui
+      installerDesktop
     ];
   };
+
+  system.activationScripts.installerDesktop = ''
+    mkdir -p /home/nixos/Desktop
+    chown nixos:users /home/nixos /home/nixos/Desktop
+    ln -sfT \
+      ${installerDesktop}/share/applications/logos-installer.desktop \
+      /home/nixos/Desktop/logos-installer.desktop
+  '';
 
   system.stateVersion = "26.05";
 }
