cb062a6d4223a4613fee59cc93d64ad2d8910a8a
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-05 22:32:39 -0400

    fix(emacs): declare ActivityWatch mode before timer hooks

---
+++ .doom.d/autoload/activity-watch.el
@@ -0,0 +1,12 @@
+;;; activity-watch.el --- Early ActivityWatch declarations -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defvar activity-watch-mode nil
+  "Non-nil when ActivityWatch is enabled in the current buffer.")
+
+;;;###autoload
+(defvar global-activity-watch-mode nil
+  "Non-nil when ActivityWatch is enabled globally.")
+
+(provide '+activity-watch-autoloads)
+;;; activity-watch.el ends here
