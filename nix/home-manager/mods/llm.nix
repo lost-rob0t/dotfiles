479873dff0d1a3d084043f46ff9134944317b499
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-05 21:37:23 -0400

    fix(home-manager): use Node.js 24 for LLM tools

--- nix/home-manager/mods/llm.nix
+++ nix/home-manager/mods/llm.nix
@@ -16,7 +16,7 @@
       inputs.zara.packages.${stdenv.hostPlatform.system}.zara-dictate
       inputs.zara.packages.${stdenv.hostPlatform.system}.zara-prolog
       inputs.org-vector.packages.${stdenv.hostPlatform.system}.org-vector
-      nodejs_22
+      nodejs_24
 
       # LLM Editors
       opencode
