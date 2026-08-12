56d36ae1e5c9f74de57a6698fcd72ee383c2f525
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-12 18:27:59 -0400

    flake: add unstable nixpkgs for ComfyUI

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     disko = {
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";
