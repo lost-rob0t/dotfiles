57a0301b95875841d666dcd7f3e3e1ba2739958e
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-20 21:34:46 -0400

    Keep GPT TODO tooling out of base module

--- nix/home-manager/mods/base.nix
+++ nix/home-manager/mods/base.nix
@@ -6,25 +6,6 @@ let
     runtimeInputs = [ pkgs.git ];
     text = builtins.readFile ../../../.config/bash/git-sync.sh;
   };
-
-  gptTodosSync = pkgs.writeShellApplication {
-    name = "gpt-todos-sync";
-    runtimeInputs = [
-      pkgs.coreutils
-      pkgs.findutils
-      pkgs.git
-      pkgs.rsync
-      pkgs.util-linux
-      pkgs.cronie
-    ];
-    text = builtins.readFile ../../../scripts/gpt-todos-sync;
-  };
-
-  installGptTodosCron = pkgs.writeShellApplication {
-    name = "install-gpt-todos-cron";
-    runtimeInputs = [ pkgs.cronie ];
-    text = builtins.readFile ../../../scripts/install-gpt-todos-cron;
-  };
 in
 {
   options = {
@@ -51,10 +32,6 @@ in
       starship
       curl
 
-    ]) ++ [
-      gitSync
-      gptTodosSync
-      installGptTodosCron
-    ];
+    ]) ++ [ gitSync ];
   };
 }
