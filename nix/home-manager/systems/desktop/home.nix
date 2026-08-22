f37a2792794c13fc9a759dd6d4d84c9ff3959718
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-22 19:19:13 -0400

    feat(nix): enable screen capture on desktop

--- nix/home-manager/systems/desktop/home.nix
+++ nix/home-manager/systems/desktop/home.nix
@@ -20,6 +20,10 @@
     enable = true;
   };
 
+  screenCapture = {
+    enable = true;
+  };
+
   prologMcp = {
     enable = true;
   };
@@ -71,7 +75,7 @@
   #
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
-  # changes in each release.
+  # changes.
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
