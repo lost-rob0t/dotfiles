d7a9f33882d54802a06b0f5702ed3cb237139a77
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-21 23:13:05 -0400

    chore: keep desktop comments unchanged

--- nix/home-manager/systems/desktop/home.nix
+++ nix/home-manager/systems/desktop/home.nix
@@ -64,14 +64,14 @@
     homeDirectory = "/home/unseen";
     stateVersion = "23.11";
   };
-  # This value determines which Home Manager release that your
+  # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
   # incompatible changes.
   #
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
-  # changes.
+  # changes in each release.
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
