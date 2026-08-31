2186eb74bb13111e9032ff9982bdefc42e06b420
N545PY <nsaspy@fedora.email>
2026-08-31 14:08:12 -0400

    llm-log: pin rev wiring capture ingest into the expert plane

--- nix/home-manager/mods/llm.nix
+++ nix/home-manager/mods/llm.nix
@@ -2,7 +2,7 @@
 
 let
   comfyui = pkgs.comfyui.override { withManager = true; };
-  llmLogRevision = "61eb86f36a4e6500dabdb2d511f2390b18650def";
+  llmLogRevision = "7b1d4359764fcc87fb967ed6e6577a9eaf6f3045";
   llmLogFlake = builtins.getFlake "github:lost-rob0t/llm-log/${llmLogRevision}";
   llmLogPackage = llmLogFlake.packages.${pkgs.stdenv.hostPlatform.system}.default;
   llmLogExpertPackage = llmLogFlake.packages.${pkgs.stdenv.hostPlatform.system}.llm-log-expert;
