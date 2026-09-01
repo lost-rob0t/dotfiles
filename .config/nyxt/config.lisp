0942f54834662f4eeba943aa9066d0ba00215808
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-31 20:23:36 -0400

    feat(nyxt): synthwave foundation for operator overhaul (#145)
    * feat(nyxt): restore synthwave theme
    
    * feat(nyxt): restore hardened scrot capture workflow
    
    * refactor(nyxt): modularize modern configuration
    
    * refactor(nyxt): simplify themed status buffer

--- .config/nyxt/config.lisp
+++ .config/nyxt/config.lisp
@@ -1,77 +1,75 @@
-;; Load slynk
+;; Nyxt configuration.
+;; The visual layer is in theme.lisp; capture/reporting commands are in
+;; reporting.lisp.
 
 (in-package :nyxt)
 
 (when (asdf:load-system :slynk)
   (define-command start-slynk (&optional (slynk-port 4006))
-    "Start a Slynk server that can be connected to, for instance, in
-Emacs via SLY.
+    "Start a Slynk server for SLY.
 
-Warning: This allows Nyxt to be controlled remotely, that is, to
-execute arbitrary code with the privileges of the user running Nyxt.
-Make sure you understand the security risks associated with this
-before running this command."
+This exposes arbitrary code execution with the privileges of the Nyxt process.
+Only run it on a trusted machine/session."
     (slynk:create-server :port slynk-port :dont-close t)
     (echo "Slynk server started at port ~a" slynk-port)))
 
-
 (in-package #:nyxt-user)
 
-;; Auto config stuff
-(define-configuration (web-buffer prompt-buffer panel-buffer
-                                  nyxt/mode/editor:editor-buffer)
-  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
-
-
-
-(define-configuration (web-buffer)
-  ((default-modes (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
-
-(define-configuration (web-buffer)
+;; Vi where navigation is useful, insert mode in the command/prompt UI.
+(define-configuration (web-buffer panel-buffer nyxt/mode/editor:editor-buffer)
   ((default-modes
-    (pushnew 'nyxt/mode/reduce-tracking:reduce-tracking-mode %slot-value%))))
+    (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
 
-(define-configuration (web-buffer)
-  ((default-modes (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
-
-
-
-;; Make the Web buffers vi mode.
 (define-configuration prompt-buffer
-  ((default-modes (append '(vi-insert-mode) %slot-default%))))
+  ((default-modes
+    (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
 
+;; Privacy defaults.
+(define-configuration web-buffer
+  ((default-modes
+    (remove-duplicates
+     (append
+      '(nyxt/mode/blocker:blocker-mode
+        nyxt/mode/reduce-tracking:reduce-tracking-mode)
+      %slot-value%)
+     :test #'eq))))
 
 (defvar *my-search-engines*
   (list
    '("google" "https://google.com/search?q=~a" "https://google.com")
+   '("brave" "https://search.brave.com/search?q=~a" "https://search.brave.com")
    '("sp" "https://www.startpage.com/do/search?query=~a" "https://www.startpage.com")
-   '("py" "https://docs.python.org/3/search.html?q=~a"
-     "https://docs.python.org/3")
+   '("gh" "https://github.com/search?q=~a" "https://github.com")
+   '("py" "https://docs.python.org/3/search.html?q=~a" "https://docs.python.org/3")
    '("cve" "https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=~a")
-   '("fec" "https://www.fec.gov/data/receipts/individual-contributions/?contributor_name=~a" "https://www.fec.gov/data/receipts/individual-contributions/")
-   '("nixpkgs" "https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=~a" "https://search.nixos.org/packages")
-   '("brave" "https://search.brave.com/search?q=~a" "https://search.brave.com/search"))
-
-  "List of search engines.")
+   '("fec"
+     "https://www.fec.gov/data/receipts/individual-contributions/?contributor_name=~a"
+     "https://www.fec.gov/data/receipts/individual-contributions/")
+   '("nixpkgs"
+     "https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=~a"
+     "https://search.nixos.org/packages"))
+  "Search engines available from the prompt.")
 
 (define-configuration context-buffer
-  "Go through the search engines above and make-search-engine out of them."
   ((search-engines
     (append
-     (mapcar (lambda (engine) (apply 'make-search-engine engine))
+     (mapcar (lambda (engine)
+               (apply #'make-search-engine engine))
              *my-search-engines*)
-     %slot-default%)))
+     %slot-default%))))
 
+(define-configuration browser
+  ((external-editor-program
+    (list "emacsclient" "-c" "-F" "'(name . \"floating\")"))))
 
-
-  ;; Use emacs
-
-  (define-configuration browser
-      ((external-editor-program (list "emacsclient" "-c" "-F" "'(name . \"floating\")")))))
-
-
+;; Retain the compositor workaround that fixed blank WebKit rendering on this
+;; setup.
 (setf (uiop:getenv "WEBKIT_DISABLE_COMPOSITING_MODE") "1")
 
-(nyxt::load-lisp "~/.config/nyxt/proxy.lisp")
+;; Load visual configuration before status configuration so the status buffer
+;; picks up the synthwave theme.
+(nyxt::load-lisp "~/.config/nyxt/theme.lisp")
 (nyxt::load-lisp "~/.config/nyxt/status.lisp")
+(nyxt::load-lisp "~/.config/nyxt/reporting.lisp")
+(nyxt::load-lisp "~/.config/nyxt/proxy.lisp")
 (nyxt::load-lisp "~/.config/nyxt/crunchbase.lisp")
--- .config/nyxt/status.lisp
+++ .config/nyxt/status.lisp
@@ -1,33 +1,41 @@
 (in-package #:nyxt-user)
 
-(define-configuration :status-buffer
-  "Display modes as short glyphs."
+(define-configuration status-buffer
   ((glyph-mode-presentation-p t)))
 
-(define-configuration :force-https-mode ((glyph "ϕ")))
-(define-configuration :user-script-mode ((glyph "u")))
-(define-configuration :blocker-mode ((glyph "β")))
-(define-configuration :proxy-mode ((glyph "π")))
-(define-configuration :reduce-tracking-mode ((glyph "τ")))
-(define-configuration :certificate-exception-mode ((glyph "χ")))
-(define-configuration :style-mode ((glyph "ϕ")))
-(define-configuration :cruise-control-mode ((glyph "σ")))
+(define-configuration :force-https-mode
+  ((glyph "ϕ")))
 
-(define-configuration status-buffer
-  "Hide most of the status elements but URL and modes."
-  ((style (str:concat
-           %slot-value%
-           (theme:themed-css (theme *browser*)))
-          `("#controls,#tabs"
-            :display none !important))))
+(define-configuration :user-script-mode
+  ((glyph "u")))
+
+(define-configuration :blocker-mode
+  ((glyph "β")))
+
+(define-configuration :proxy-mode
+  ((glyph "π")))
+
+(define-configuration :reduce-tracking-mode
+  ((glyph "τ")))
+
+(define-configuration :certificate-exception-mode
+  ((glyph "χ")))
+
+(define-configuration :style-mode
+  ((glyph "ϕ")))
+
+(define-configuration :cruise-control-mode
+  ((glyph "σ")))
 
 (defmethod format-status-load-status ((status status-buffer))
-  "A fancier load status."
+  "Display a compact loading state."
+  (declare (ignore status))
   (spinneret:with-html-string
-      (:span (if (and (current-buffer)
-                      (web-buffer-p (current-buffer)))
-                 (case (slot-value (current-buffer) 'nyxt::status)
-                   (:unloaded "∅")
-                   (:loading "∞")
-                   (:finished ""))
-                 ""))))
+    (:span
+     (if (and (current-buffer)
+              (web-buffer-p (current-buffer)))
+         (case (slot-value (current-buffer) 'nyxt::status)
+           (:unloaded "∅")
+           (:loading "∞")
+           (:finished ""))
+         ""))))
