19277c211581fafa641276ffa8c5f8582df0c9f7
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-07 02:30:41 -0400

    Keep Zara listener change scoped

--- nix/home-manager/systems/desktop/home.nix
+++ nix/home-manager/systems/desktop/home.nix
@@ -106,8 +106,9 @@
   # when a new Home Manager release introduces backwards
   # incompatible changes.
   #
-  # You can update this value in your configuration without breakage
-  # implications and the Home Manager release notes should be read first.
+  # You can update this value in your configuration without breakage.
+  # See the Home Manager release notes for a list of state version
+  # changes.
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
