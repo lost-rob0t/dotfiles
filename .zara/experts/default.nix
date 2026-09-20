be6a2119e57819c20d09165e34501d074310b947
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-20 12:27:16 -0400

    test(experts): run DotfilesExpert in expert library gate

--- .zara/experts/default.nix
+++ .zara/experts/default.nix
@@ -21,6 +21,7 @@ let
 
       swipl -q -f none -s .zara/experts/git/tests/expert-tests.pl
       swipl -q -f none -s .zara/experts/home-manager/tests/expert-tests.pl
+      swipl -q -f none -s .zara/experts/dotfiles/tests/expert-tests.pl
       swipl -q -f none -s .zara/experts/zara/tests/expert-tests.pl
       swipl -q -f none -s .zara/experts/sysadmin/tests/expert-tests.pl
       swipl -q -f none -s .zara/experts/javascript/tests/expert-tests.pl
