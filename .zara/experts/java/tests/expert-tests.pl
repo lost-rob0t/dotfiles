761d1a45827045894e7bb3d910dfca3eadc764d1
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-20 00:51:25 -0400

    test(experts): cover JavaExpert JVM and Android boundaries

---
+++ .zara/experts/java/tests/expert-tests.pl
@@ -0,0 +1,24 @@
+:- use_module(library(plunit)).
+:- use_module('../kb/expert').
+
+:- begin_tests(dotfiles_java_expert).
+test(identity_and_upstream) :-
+    expert_id('zara:expert/java'),
+    upstream_contract('lost-rob0t/prolog-rlm#501').
+test(java_is_accepted) :- accepts_extension(java).
+test(kotlin_is_not_silently_accepted, [fail]) :- accepts_extension(kt).
+test(java_language_features_are_explicit) :-
+    supports_semantic(generics),
+    supports_semantic(records),
+    supports_semantic(modules).
+test(kotlin_coroutines_are_not_java_semantics, [fail]) :- supports_semantic(coroutines).
+test_project_metadata_is_observation_only) :-
+    project_metadata_role(observation_only),
+    android_metadata_role(observation_only).
+test(generated_sources_are_excluded) :- generated_source_policy(exclude).
+test(repairs_require_parse_and_compile_verification) :- repair_verification(reparse_and_compile).
+test(zero_model) :- model_calls(0).
+:- end_tests(dotfiles_java_expert).
+
+:- initialization(main, main).
+main :- (run_tests -> halt(0) ; halt(1)).
