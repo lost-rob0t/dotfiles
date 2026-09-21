47b7c0765a25d4d0576bf823404e67a98835a8a7
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-20 20:26:55 -0400

    test(todoExpert): add deterministic projection fixture (#307)

---
+++ .zara/experts/todo/tests/fixtures/todo-kb.pl
@@ -0,0 +1,4 @@
+% Generated fixture.
+todo_generation('2026-09-21T00:00:00+0000').
+todo('task-next','NEXT','Ship Mara bridge','/notes/inbox.org','<2026-09-23 Wed 08:00>','','A','zara,mara').
+todo('task-done','DONE','Old task','/notes/archive.org','','<2026-09-20 Sun>','','archive').
